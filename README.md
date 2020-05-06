64dedbeff180db558bcf86bfea2281126559217e
dpolve <dawde.din@gmail.com>
2020-05-06 10:56:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ifts-verifica-2-20200506-ayv38z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ifts-verifica-2-20200506-ayv38z)
---
+++ src/app/dati.model.ts
@@ -0,0 +1,15 @@
+class Regione {
+  campo1: string;
+  campo2: string;
+}
+
+export const dati = [
+    {
+      campo1: "valore1",
+      campo2: "valore2"
+    },
+    {
+      campo1: "valore1_2",
+      campo2: "valore2_2"
+    },
+  ];
